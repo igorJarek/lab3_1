18849e5ca9d5e864d3c3e02ec5d0fb0f39aa0359
Igor <igij@o2.pl>
2019-04-23 22:08:07 +0200

    isolation Lab 3_1 - 1.1 Add Test case 2

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -31,7 +31,7 @@ public class BookKeeperTest {
     }
 
     @Test
-    public void invoiceRequestWIthOneItemMustReturnInvoiceWithOneItem() {
+    public void invoiceRequestWithOneItemMustReturnInvoiceWithOneItem() {
         InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
         ProductData productData = mock(ProductData.class);
         RequestItem requestItem = new RequestItem(productData, 1, Money.ZERO);
@@ -46,5 +46,29 @@ public class BookKeeperTest {
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
 
         assertThat(invoice.getItems().size(), is(1));
+    }
+
+    @Test
+    public void invoiceRequestWithTwoItemMustCallCalculateTax2Times() {
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        ProductData productDataOne = mock(ProductData.class);
+        ProductData productDataTwo = mock(ProductData.class);
+        RequestItem requestItemOne = new RequestItem(productDataOne, 1, new Money(10));
+        RequestItem requestItemTwo = new RequestItem(productDataTwo, 1, new Money(20));
+
+        when(productDataOne.getType()).thenReturn(ProductType.STANDARD);
+        when(productDataTwo.getType()).thenReturn(ProductType.FOOD);
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class)))
+                .thenReturn(new Tax(Money.ZERO, null));
+
+        invoiceRequest.add(requestItemOne);
+        invoiceRequest.add(requestItemTwo);
+
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        verify(taxPolicy, times(1)).calculateTax(productDataOne.getType(), requestItemOne.getTotalCost());
+        verify(taxPolicy, times(1)).calculateTax(productDataTwo.getType(), requestItemTwo.getTotalCost());
     }
 }
