dbe47d438ef32b5c85ea0e053f8358bb15a7f688
Igor <igij@o2.pl>
2019-05-09 00:53:26 +0200

    Lab 3_1 - 1.2 Add Behavior and State Test 1

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,66 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.*;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.*;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.*;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation.ReservationStatus;
+import pl.com.bottega.ecommerce.sales.domain.reservation.*;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class AddProductCommandHandlerTest {
+
+    private AddProductCommand addProductCommand;
+    private ReservationRepository reservationRepository;
+    private ProductRepository productRepository;
+    private SuggestionService suggestionService;
+    private ClientRepository clientRepository;
+    private SystemContext systemContext;
+    private AddProductCommandHandler productCommandHandler;
+    private Reservation reservation;
+    private Product removedProduct;
+    private Product availableProduct;
+    private Client client;
+
+    @Before
+    public void setUp() {
+        addProductCommand = new AddProductCommand(Id.generate(), Id.generate(), 1);
+
+        reservationRepository = mock(ReservationRepository.class);
+        productRepository = mock(ProductRepository.class);
+        suggestionService = mock(SuggestionService.class);
+        clientRepository = mock(ClientRepository.class);
+        systemContext = new SystemContext();
+
+        productCommandHandler = new AddProductCommandHandler(reservationRepository, productRepository, suggestionService, clientRepository, systemContext);
+        reservation = new Reservation(Id.generate(), ReservationStatus.OPENED, new ClientData(Id.generate(), "Kowalski"), new Date(2000, 1, 1));
+        removedProduct = new Product(Id.generate(), new Money(10), "Papier toaletowy", ProductType.STANDARD);
+        removedProduct.markAsRemoved();
+        availableProduct = new Product(Id.generate(), new Money(20), "Papier ścierny", ProductType.STANDARD);
+        client = new Client();
+    }
+
+    @Test
+    public void addOneProductShouldReturnReservationWithOneProduct() {
+        when(reservationRepository.load(addProductCommand.getOrderId())).thenReturn(reservation);
+        when(productRepository.load(addProductCommand.getProductId())).thenReturn(availableProduct);
+
+        doNothing().when(reservationRepository).save(reservation);
+
+        productCommandHandler.handle(addProductCommand);
+
+        assertThat(reservation.getReservedProducts().size(), is(1));
+        verifyZeroInteractions(suggestionService);
+        verifyZeroInteractions(clientRepository);
+    }
+}
